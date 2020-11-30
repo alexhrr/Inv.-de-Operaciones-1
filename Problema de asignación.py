1d65955c19128cd5df7d86292440a8375a34c687
ssalinasr <54372344+ssalinasr@users.noreply.github.com>
2020-11-30 12:19:59 -0500

    Código problema de asignación

---
+++ Problema de asignación.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 30 12:12:17 2020
+
+@author: FliaSalinasRodriguez
+"""
+
+#Se importa la librería del Algoritmo Húngaro
+from hungarian_algorithm import algorithm
+
+#Creamos un grafo con los empleados, sus actividades y los costos respectivos
+g = {
+     'Empleado 1': {'C': 6, 'T': 8, 'A': 7},
+     'Empleado 2': {'C': 7, 'T': 6, 'A': 8},
+     'Empleado 3': {'C': 6, 'T': 8, 'A': 8}
+     }
+
+#Se asigna la tarea a cada empleado, cuyo costo es MENOR
+res = algorithm.find_matching(g, matching_type = 'min', return_type = 'list' )
+
+#Imprimimos los resultados
+print(res)
