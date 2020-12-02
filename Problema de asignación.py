ac35b210d9271930e21dc5322d453bd7b73b49ef
ssalinasr <54372344+ssalinasr@users.noreply.github.com>
2020-12-02 14:41:05 -0500

    Código problema de asignación

--- Problema de asignación.py
+++ Problema de asignación.py
@@ -22,5 +22,27 @@ res = algorithm.find_matching(g, matching_type = 'min', return_type = 'list' )
 cost =  algorithm.find_matching(g, matching_type = 'min', return_type = 'total' )
 
 #Imprimimos los resultados
+print()
+print(res)
+print('La empresa pagaría, según esta asignación, ' + str(cost) + ' horas')
+print()
+
+'Ejemplo 2'
+
+#Creamos un grafo con los empleados, sus actividades y los costos respectivos
+g = {
+     'Empleado 1': {'C': 1, 'T': 4,'D': 6, 'A': 3},
+     'Empleado 2': {'C': 9, 'T': 7,'D': 10, 'A': 9},
+     'Empleado 3': {'C': 4, 'T': 5,'D': 11, 'A': 7},
+     'Empleado 4': {'C': 8, 'T': 7,'D': 8, 'A': 5}
+     }
+
+#Se asigna la tarea a cada empleado, cuyo costo es MENOR
+res = algorithm.find_matching(g, matching_type = 'min', return_type = 'list' )
+
+#Encontramos el costo mínimo
+cost =  algorithm.find_matching(g, matching_type = 'min', return_type = 'total' )
+
+#Imprimimos los resultados
 print(res)
 print('La empresa pagaría, según esta asignación, ' + str(cost) + ' horas')
